983bb71da5d549f6adc3897a2d5f9ce5e52361f2
Brian Woodward <brian.woodward@gmail.com>
2016-07-29 15:23:01 -0400

    add .verb.md file add verb config to package.json add code comments

--- index.js
+++ index.js
@@ -4,7 +4,30 @@ var File = require('vinyl');
 var through = require('through2');
 var groupBy = require('group-array');
 
-module.exports = function() {
+/**
+ * Group vinyl files by specified properties on the vinyl files.
+ * This uses [group-array][] to do the grouping after all the vinyl files have come through the stream.
+ * See the [group-array][] documentation for more advanced features.
+ *
+ * ```js
+ * gulp.task('group', function() {
+ *   return gulp.src(['path/to/src/files/*.js'])
+ *     .pipe(groupArray('dirname', {
+ *       groupFn: function(group) { console.log(group); }
+ *     }));
+ * });
+ * ```
+ *
+ * @param  {Mixed}    `args...` All of the arguments will be passed through to [group-array][] except for the last argument if it's an object.
+ * @param  {Object}   `options` Optional object to specify options. If this is passed, then it won't be passed to [group-array][].
+ * @param  {Function} `options.groupFn` Function that will be called with the `group` object. This is the results from calling [group-array][].
+ * @param  {Boolean}  `options.flush` When set to `false`, the source files will not be pushed through the stream. (Defaults to `true`)
+ * @param  {Boolean}  `options.groupFile` When set to `true`, a new vinyl file will be created with a `.group` property containing the group object created by [group-array][].
+ * @returns {Stream}  Returns a stream to pipe vinyl source files through.
+ * @api public
+ */
+
+module.exports = function groupArray() {
   var files = [];
   var options = {};
   var args = [].slice.call(arguments);
